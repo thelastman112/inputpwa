9b24473231f91f4e1fa36c6bcc262296d0c00434
thelastman112 <rizkymaestro12@gmail.com>
2022-12-20 14:38:36 +0700

    add post

---
+++ pwa.js
@@ -0,0 +1,6 @@
+// setting up PWA
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js')
+        .then((reg) => console.log('serviceworker registered', reg))
+        .catch((err) => console.log('serviceworker failed to register', err));
+  }
